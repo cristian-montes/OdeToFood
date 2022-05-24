880355ad2d89294c147ac77028eee86d9e417a9f
Montes <cristian.montes@capgemini.com>
2022-05-24 09:37:14 -0600

    Added foulder OdeToFood.Data and the class file named IRestaurantData.cs and most of the logic in there with hardcoded data to begin displying data in the page

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Entities;
+
+namespace OdeToFood.OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        //Declaring this the data type and structure of the List
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Fort Collins", Cuisine=CuisineType.Itilian},
+                new Restaurant { Id = 2, Name = "Tacohuaches", Location = "Huston", Cuisine=CuisineType.Mexican},
+                new Restaurant { Id = 3, Name = "Pollo Curry", Location = "New York", Cuisine=CuisineType.Indian}
+            };
+
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+           return from r in restaurants
+                  orderby r.Name
+                  select r;
+        }
+    }
+}
